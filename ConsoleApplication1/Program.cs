e61573b9ff4ad6781c81ca430c61f98f91b75d1b
Eldar Dordzhiev <eldardordzhiev@gmail.com>
2016-06-14 14:20:32 +0300

    Add project files.

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        public class KpsData
+        {
+            public IList<Zone> Zones { get; set; }
+        }
+        public class Geoposition
+        {
+            public double Latitude { get; set; }
+
+            public double Longitude { get; set; }
+        }
+
+        public class Zone
+        {
+            public string Name { get; set; }
+
+            public IList<Geoposition> Positions { get; set; }
+
+            public double ThreatLevel { get; set; }
+        }
+
+        static void Main(string[] args)
+        {
+            var json = File.ReadAllText(@"C:\Users\Desktop1\Desktop\a.json");
+            var data = JsonConvert.DeserializeObject<KpsData>(json);
+
+            var lod = 11;
+            var lodDelta = 2;
+            lod += lodDelta;
+            var lodPower = (int) Math.Pow(2, lodDelta);
+            var startXTile = 1236 * lodPower;
+            var endXTile = 1240 * lodPower;
+            var startYTile = 636 * lodPower;
+            var endYTile = 643 * lodPower;
+            int startY;
+            int startX;
+            TileSystem.TileXYToPixelXY(startXTile, startYTile, out startX, out startY);
+            int endX;
+            int endY;
+            TileSystem.TileXYToPixelXY(endXTile, endYTile, out endX, out endY);
+            var bitmap = new Bitmap(endX - startX, endY - startY);
+            var graphics = Graphics.FromImage(bitmap);
+            graphics.Clear(Color.Transparent);
+
+            foreach (var zone in data.Zones)
+            {
+                var points = new List<Point>();
+                foreach (var geoposition in zone.Positions)
+                {
+                    long x;
+                    long y;
+                    TileSystem.LatLongToPixelXY(geoposition.Latitude, geoposition.Longitude, lod, out x, out y);
+
+                    x -= startX;
+                    y -= startY;
+
+                    if (x < 0 || y < 0 || x >= bitmap.Width || y >= bitmap.Height)
+                    {
+                        points = null;
+                        break;
+                    }
+
+                    points.Add(new Point((int) x, (int) y));
+                }
+
+                if (points == null)
+                {
+                    continue;
+                }
+
+                var threatLevel = zone.ThreatLevel;
+                var color = threatLevel < 0.5
+                    ? ColorInterpolator.InterpolateBetween(Color.LawnGreen, Color.Yellow, threatLevel * 2)
+                    : ColorInterpolator.InterpolateBetween(Color.Yellow, Color.Red, (threatLevel - 0.5) * 2);
+                color = Color.FromArgb(100, color);
+                graphics.FillPolygon(new SolidBrush(color), points.ToArray());
+            }
+
+            graphics.Dispose();
+            bitmap.Save(@"C:\Users\Desktop1\Desktop\out.png", ImageFormat.Png);
+        }
+    }
+}
---
+++ KpsServer/Controllers/MapController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Web;
+using System.Web.Hosting;
+using System.Web.Http;
+
+namespace KpsServer.Controllers
+{
+    public class MapController : ApiController
+    {
+        static Image map = Image.FromFile(HostingEnvironment.MapPath("~/App_Data/out.png"));
+
+        public HttpResponseMessage Get(int zoomLevel, int x, int y)
+        {
+            var lod = 11;
+            var lodDelta = 2;
+            lod += lodDelta;
+            var lodPower = (int)Math.Pow(2, lodDelta);
+            var startXTile = 1236 * lodPower;
+            var startYTile = 636 * lodPower;
+            int startY;
+            int startX;
+            TileSystem.TileXYToPixelXY(startXTile, startYTile, out startX, out startY);
+
+            var x1 = x * 256 * Math.Pow(2, lod - zoomLevel);
+            var y1 = y * 256 * Math.Pow(2, lod - zoomLevel);
+            var w = 256 * Math.Pow(2, lod - zoomLevel);
+            
+            var bitmap = new Bitmap(256, 256);
+            using (var graphics = Graphics.FromImage(bitmap))
+            {
+                graphics.Clear(Color.Transparent);
+                lock (map)
+                {
+                    graphics.DrawImage(map, new Rectangle(0, 0, 256, 256), (float) (x1 - startX), (float)(y1 - startY), (float)w, (float)w, GraphicsUnit.Pixel);
+                }
+            }
+
+            var stream = new MemoryStream();
+            bitmap.Save(stream, ImageFormat.Png);
+            stream.Position = 0;
+
+            var response = new HttpResponseMessage();
+            response.Content = new StreamContent(stream);
+            response.Content.Headers.ContentType = MediaTypeHeaderValue.Parse("image/png");
+            return response;
+        }
+    }
+}
